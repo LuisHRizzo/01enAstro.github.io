75a3f83bc47f05c3c7f99fe6e4c43d64a5390add
LuisHRizzo <70038673+LuisHRizzo@users.noreply.github.com>
2024-01-29 15:04:29 -0300

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://01emAstro.github.io',
+  site: 'https://01enAstro.github.io',
   base: SITE.basePathname,
   compressHTML: true,
   trailingSlash: 'always',
